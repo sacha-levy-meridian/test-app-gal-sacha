54b40f1b9042024a5cc6eda7375820d3e0d6e6ea
Sacha Levy <sacha@sacha-portfolio.com>
2026-07-02 23:13:28 +0300

    Update database configuration to use MySQL and modify Session table schema for compatibility

--- prisma/migrations/20240530213853_create_session_table/migration.sql
+++ prisma/migrations/20240530213853_create_session_table/migration.sql
@@ -1,20 +1,22 @@
 -- CreateTable
-CREATE TABLE "Session" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "shop" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
-    "isOnline" BOOLEAN NOT NULL DEFAULT false,
-    "scope" TEXT,
-    "expires" DATETIME,
-    "accessToken" TEXT NOT NULL,
-    "userId" BIGINT,
-    "firstName" TEXT,
-    "lastName" TEXT,
-    "email" TEXT,
-    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
-    "locale" TEXT,
-    "collaborator" BOOLEAN DEFAULT false,
-    "emailVerified" BOOLEAN DEFAULT false,
-    "refreshToken" TEXT,
-    "refreshTokenExpires" DATETIME
-);
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `shop` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `isOnline` BOOLEAN NOT NULL DEFAULT false,
+    `scope` VARCHAR(191) NULL,
+    `expires` DATETIME(3) NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `userId` BIGINT NULL,
+    `firstName` VARCHAR(191) NULL,
+    `lastName` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `accountOwner` BOOLEAN NOT NULL DEFAULT false,
+    `locale` VARCHAR(191) NULL,
+    `collaborator` BOOLEAN NULL DEFAULT false,
+    `emailVerified` BOOLEAN NULL DEFAULT false,
+    `refreshToken` VARCHAR(191) NULL,
+    `refreshTokenExpires` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
